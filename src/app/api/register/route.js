78e4dfdb7bbdce481d807cdac59032dcf756fc2b
Kekbabus <b00148239@mytudublin.ie>
2023-11-30 10:23:59 +0000

    register redirects to login page after success

--- src/app/api/register/route.js
+++ src/app/api/register/route.js
@@ -38,6 +38,7 @@ export async function GET(req, res) {
             "telephone" : telephone,
             "dob" : dob
         }).toArray();
+        window.location.href ='dashboard'
         
     }
     //==========================================================
