cb64977abed67a69f5fc4c1618b25818f42074f2
Kekbabus <b00148239@mytudublin.ie>
2023-12-18 21:06:19 +0000

    added email-validator-pass1

--- src/app/page.js
+++ src/app/page.js
@@ -9,7 +9,11 @@ import Checkbox from "@mui/material/Checkbox";
 import Link from "@mui/material/Link";
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
-
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import { ThemeProvider } from "@mui/material/styles";
